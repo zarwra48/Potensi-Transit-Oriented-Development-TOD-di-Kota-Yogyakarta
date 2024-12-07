0cde0714e50f81df2947b208dd9f2ad5d6366fbc
zarwra48 <azzarand07@gmail.com>
2024-12-08 02:33:40 +0700

    Add files via upload

---
+++ data/Exportedstasiuntugu_15.js
@@ -0,0 +1,1 @@
+var json_Exportedstasiuntugu_15 = {"type":"FeatureCollection","name":"Exportedstasiuntugu_15","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.363506782051061,-7.789256898783512]}}]}
